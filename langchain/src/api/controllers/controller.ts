97508fb799c64f2903498dec72ef132f1f632910
bfpill <max@v3rv.com>
2023-05-27 22:56:50 +1000

    Turned langchain agent into a nice class : ( )
    Now we can send any question we want to any number of compilers and agents with some JSON.
    
    Next step is to enable agent to create compiler instances and then talk to them in any language it wants.
    
    + removed unused imports and old agents

--- langchain/src/api/controllers/controller.ts
+++ langchain/src/api/controllers/controller.ts
@@ -23,37 +23,6 @@ const newChatMessage = async (req, res) => {
     res.status(201).send({ status: "OK", data: {messageResponse} });
 };
 
-const init = async (req: any, res: any) => {
-    const { body } = req;
-    if (
-        !body.key
-    ) {
-        res
-            .status(400)
-            .send({
-                status: "FAILED",
-                data: {
-                    error:
-                        "Error occurred because you failed to provide a clone key or a message"
-                },
-            });
-        return;
-    }
-    const {key} = body;
-    const result = await basicServices.init(key);
-
-    res.status(201).send({ status: "OK", data: {result} });
-};
-
-const run = async (req: any, res: any) => {
-    const result = await basicServices.run();
-
-    res.status(201).send({ status: "OK", data: {result} });
-};
-
-
 export default {
     newChatMessage,
-    init, 
-    run
 };
